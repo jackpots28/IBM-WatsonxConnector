5d2df50b2792f0cc5cb254a0072a78f6b060709d
jackpots28 <xblackjack28x@gmail.com>
2024-09-11 13:54:49 -0500

    Quick commit before meeting

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import requests
 import typing
 import os
 
+from loguru import logger
+
 
 class WatsonxConnector(object):
     __slots__ = [
@@ -11,10 +13,12 @@ class WatsonxConnector(object):
         '_c_priv_model_id',
         '_c_priv_api_version',
         '_c_priv_full_url',
+        '_c_priv_project_id',
         '_c_priv_model_params',
         'c_base_url',
         'c_usr_name',
         'c_api_key',
+        'c_api_token',
     ]
 
     def __init__(self, base_url: str, usr_name: str, api_key: str, ):
@@ -22,12 +26,14 @@ class WatsonxConnector(object):
         self.c_base_url: str = base_url
         self.c_usr_name: str = usr_name
         self.c_api_key: str = api_key
+        self.c_api_token: str = self.generate_auth_token()
         #   --- Protected Vars
         self._c_priv_sys_prompt: str = ""
         self._c_priv_api_base: str = ""
         self._c_priv_model_id: str = ""
         self._c_priv_api_version: str = ""
         self._c_priv_full_url: str = ""
+        self._c_priv_project_id: str = ""
         self._c_priv_model_params: dict = dict()
 
     def __str__(self):
@@ -43,10 +49,10 @@ class WatsonxConnector(object):
     def set_model_id(self, i_model_id: str):
         self._c_priv_model_id = i_model_id
 
-    def set_model_params(self, i_repetition_penalty: float, i_decoding_method: str, i_temperature: float, i_top_k: int,
+    def set_model_params(self, i_repetition_penalty: float, i_temperature: float, i_top_k: int,
                          i_top_p: int, i_max_new_tokens: int,):
         self._c_priv_model_params = {
-            "decoding_method": i_decoding_method,
+            "decoding_method": "sample",
             "max_new_tokens": i_max_new_tokens,
             "temperature": i_temperature,
             "top_k": i_top_k,
@@ -57,6 +63,9 @@ class WatsonxConnector(object):
     def set_api_version(self, i_api_version: str):
         self._c_priv_api_version = i_api_version
 
+    def set_project_id(self, i_project_id: str):
+        self._c_priv_project_id = i_project_id
+
     #   --- Getters
     def get_api_base(self) -> str:
         return self._c_priv_api_base
@@ -65,27 +74,53 @@ class WatsonxConnector(object):
         return self._c_priv_model_id
 
     #   --- UTILS
-    def text_generation(self, i_api_version: str | None, i_model_id: str | None, i_sys_prompt: str | None) -> str:
+    def text_generation(self, *args, **kwargs) -> str:
+        i_api_version: str = kwargs['i_api_version']
+        i_model_id: str = kwargs['i_model_id']
+        i_sys_prompt: str = kwargs['i_sys_prompt']
+        i_model_params: dict = kwargs['i_model_params']
+        i_project_id: str = kwargs['i_project_id']
+
+        #   --- Verify vars are set if needed
         if self._c_priv_api_version == "":
             if i_api_version is not None:
                 self.set_api_version(i_api_version)
+            else:
+                logger.error("API VERSION - not set")
         if self._c_priv_model_id == "":
             if i_model_id is not None:
                 self.set_model_id(i_model_id)
+            else:
+                logger.error("Model ID - not set")
         if self._c_priv_sys_prompt == "":
             if i_sys_prompt is not None:
                 self.set_system_prompt(i_sys_prompt)
+            else:
+                logger.error("SYSTEM PROMPT not set")
+        if self._c_priv_model_params is {}:
+            if i_model_params is not None:
+                self.set_model_params(i_model_params)
+            else:
+                logger.error("MODEL PARAMS not set")
+        if self._c_priv_project_id == "":
+            if i_project_id is not None:
+                self.set_project_id(i_project_id)
+            else:
+                logger.error("PROJECT ID not set")
 
         self._c_priv_api_base = f"{self.c_base_url}/ml/v1/text/generation?{self._c_priv_api_version}"
 
         headers = {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.get_api_base()}"
+            "Authorization": f"Bearer {self.c_api_token}"
         }
 
         body = {
-            "input": f"{self._c_priv_sys_prompt}"
+            "input": f"""{self._c_priv_sys_prompt}""",
+            "parameters": self._c_priv_model_params,
+            "model_id": self._c_priv_model_id,
+            "project_id": self._c_priv_project_id,
         }
 
         response = requests.post(
@@ -113,15 +148,14 @@ class WatsonxConnector(object):
 
 def main():
     conn = WatsonxConnector(
-        base_url=os.environ["BASE_URL"],
-        usr_name=os.environ["USER_NAME"],
-        api_key=os.environ["API_KEY"]
+        base_url="test.somedomain.com",
+        usr_name="test-user-1",
+        api_key="some_api_key_string"
     )
 
     conn.set_system_prompt(i_system_prompt="SOMETHING")
     conn.set_model_id(i_model_id="TEST_MODEL")
-
-    print(conn.generate_auth_token())
+    # print(conn.generate_auth_token())
     print(conn.get_model_id())
 
 
