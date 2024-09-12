00e97ecdaee33ff0b9796d0495a05b4ed715beb8
jackpots28 <xblackjack28x@gmail.com>
2024-09-11 23:06:42 -0500

    Refactor all arguments for methods to be more inclusive - changed required arguments for constructor - renamed vars

--- src/main.py
+++ src/main.py
@@ -1,146 +1,121 @@
-from dataclasses import dataclass
 import requests
-import typing
-import os
-
-from loguru import logger
 
 
 class WatsonxConnector(object):
     __slots__ = [
-        '_c_priv_sys_prompt',
-        '_c_priv_api_base',
-        '_c_priv_model_id',
-        '_c_priv_api_version',
-        '_c_priv_full_url',
-        '_c_priv_project_id',
-        '_c_priv_model_params',
-        'c_base_url',
-        'c_usr_name',
-        'c_api_key',
-        'c_api_token',
+        # --- Set during methods
+        '_priv_sys_prompt',
+        '_priv_api_version',
+        '_priv_full_url',
+        '_priv_model_params',
+        '_priv_api_token',
+        # --- Required on obj creation
+        "project_id",
+        'base_url',
+        'user_name',
+        'api_key',
+        'model_id',
     ]
 
-    def __init__(self, base_url: str, usr_name: str, api_key: str, ):
+    def __init__(self, base_url: str, user_name: str, api_key: str, model_id: str, project_id: str):
         #   --- Public Vars
-        self.c_base_url: str = base_url
-        self.c_usr_name: str = usr_name
-        self.c_api_key: str = api_key
-        self.c_api_token: str = self.generate_auth_token()
+        self.base_url: str = base_url
+        self.user_name: str = user_name
+        self.api_key: str = api_key
+        self._priv_api_token: str = self.generate_auth_token()
+        self.model_id: str = model_id
         #   --- Protected Vars
-        self._c_priv_sys_prompt: str = ""
-        self._c_priv_api_base: str = ""
-        self._c_priv_model_id: str = ""
-        self._c_priv_api_version: str = ""
-        self._c_priv_full_url: str = ""
-        self._c_priv_project_id: str = ""
-        self._c_priv_model_params: dict = dict()
-
-    def __str__(self):
-        return
-
-    def __repr__(self):
-        return
+        # Default system prompt - can be updated with set_system_prompt()
+        self._priv_sys_prompt: str = """You are Granite Chat, an AI language model developed by IBM. You are a 
+        cautious assistant. You carefully follow instructions. You are helpful and harmless and you follow ethical 
+        guidelines and promote positive behavior. You always respond to greetings (for example, hi, hello, g'\''day, 
+        morning, afternoon, evening, night, what'\''s up, nice to meet you, sup, etc) with \"Hello! I am Granite 
+        Chat, created by IBM. How can I help you today?\". Please do not say anything else and do not start a 
+        conversation. ----------------"""
+        self._priv_api_version: str = ""
+        self._priv_full_url: str = ""
+        self.project_id: str = project_id
+        # Default model parameters - can be updated with set_model_params()
+        self._priv_model_params: dict = {
+            "decoding_method": "sample",
+            "max_new_tokens": 2000,
+            "temperature": 0.5,
+            "top_k": 20,
+            "top_p": 1,
+            "repetition_penalty": 1.0
+        }
 
     #   --- Setters
     def set_system_prompt(self, i_system_prompt: str):
-        self._c_priv_sys_prompt = i_system_prompt
+        self._priv_sys_prompt = i_system_prompt
 
     def set_model_id(self, i_model_id: str):
-        self._c_priv_model_id = i_model_id
+        self.model_id = i_model_id
 
-    def set_model_params(self, i_repetition_penalty: float, i_temperature: float, i_top_k: int,
-                         i_top_p: int, i_max_new_tokens: int,):
-        self._c_priv_model_params = {
+    def set_model_params(self, *args, **kwargs):
+        self._priv_model_params = {
             "decoding_method": "sample",
-            "max_new_tokens": i_max_new_tokens,
-            "temperature": i_temperature,
-            "top_k": i_top_k,
-            "top_p": i_top_p,
-            "repetition_penalty": i_repetition_penalty
+            "max_new_tokens": kwargs['max_new_tokens'] | self._priv_model_params['max_new_tokens'],
+            "temperature": kwargs['temperature'] | self._priv_model_params['temperature'],
+            "top_k": kwargs['top_k'] | self._priv_model_params['top_k'],
+            "top_p": kwargs['top_p'] | self._priv_model_params['top_p'],
+            "repetition_penalty": kwargs['repetition_penalty'] | self._priv_model_params['repetition_penalty'],
         }
 
-    def set_api_version(self, i_api_version: str):
-        self._c_priv_api_version = i_api_version
+    def set_api_version(self, api_version: str):
+        self._priv_api_version = api_version
 
-    def set_project_id(self, i_project_id: str):
-        self._c_priv_project_id = i_project_id
+    def set_project_id(self, project_id: str):
+        self.project_id = project_id
 
     #   --- Getters
-    def get_api_base(self) -> str:
-        return self._c_priv_api_base
 
     def get_model_id(self) -> str:
-        return self._c_priv_model_id
+        return self.model_id
 
     #   --- UTILS
-    def text_generation(self, *args, **kwargs) -> str:
-        i_api_version: str = kwargs['i_api_version']
-        i_model_id: str = kwargs['i_model_id']
-        i_sys_prompt: str = kwargs['i_sys_prompt']
-        i_model_params: dict = kwargs['i_model_params']
-        i_project_id: str = kwargs['i_project_id']
+    def generate_text(self, query: str, *args, **kwargs) -> str:
+        input_query: str = query
+        api_version: str = kwargs['api_version'] | "2023-05-29"
+        model_id: str = kwargs['model_id'] | self.model_id
+        sys_prompt: str = kwargs['sys_prompt'] | self._priv_sys_prompt
+        model_params: dict = kwargs['model_params'] | self._priv_model_params
+        project_id: str = kwargs['project_id'] | self.project_id
 
-        #   --- Verify vars are set if needed
-        if self._c_priv_api_version == "":
-            if i_api_version is not None:
-                self.set_api_version(i_api_version)
-            else:
-                logger.error("API VERSION - not set")
-        if self._c_priv_model_id == "":
-            if i_model_id is not None:
-                self.set_model_id(i_model_id)
-            else:
-                logger.error("Model ID - not set")
-        if self._c_priv_sys_prompt == "":
-            if i_sys_prompt is not None:
-                self.set_system_prompt(i_sys_prompt)
-            else:
-                logger.error("SYSTEM PROMPT not set")
-        if self._c_priv_model_params is {}:
-            if i_model_params is not None:
-                self.set_model_params(i_model_params)
-            else:
-                logger.error("MODEL PARAMS not set")
-        if self._c_priv_project_id == "":
-            if i_project_id is not None:
-                self.set_project_id(i_project_id)
-            else:
-                logger.error("PROJECT ID not set")
-
-        self._c_priv_api_base = f"{self.c_base_url}/ml/v1/text/generation?{self._c_priv_api_version}"
+        self._priv_full_url = f"{self.base_url}/ml/v1/text/generation?version={api_version}"
 
         headers = {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {self.c_api_token}"
+            "Authorization": f"Bearer {self._priv_api_token}"
         }
 
         body = {
-            "input": f"""{self._c_priv_sys_prompt}""",
-            "parameters": self._c_priv_model_params,
-            "model_id": self._c_priv_model_id,
-            "project_id": self._c_priv_project_id,
+            "input": f"""{sys_prompt}\nQuery: {input_query}""",
+            "parameters": model_params,
+            "model_id": model_id,
+            "project_id": project_id,
         }
 
         response = requests.post(
-            self._c_priv_api_base,
+            self._priv_full_url,
             headers=headers,
             json=body
         )
-
-        return "TEST"
+        if response.status_code != 200:
+            raise Exception("Non-200 response: " + str(response.text))
+        return response.json()
 
     def generate_auth_token(self) -> str:
         return requests.post(
-            url=f"https://{self.c_base_url}/icp4d-api/v1/authorize",
+            url=f"https://{self.base_url}/icp4d-api/v1/authorize",
             headers={
                 "cache-control": "no-cache",
                 "Content-Type": "application/json"
             },
             json={
-                "username": f"{self.c_usr_name}",
-                "api_key": f"{self.c_api_key}"
+                "username": f"{self.user_name}",
+                "api_key": f"{self.api_key}"
             },
             verify=False
         ).json()["token"]
@@ -149,14 +124,13 @@ class WatsonxConnector(object):
 def main():
     conn = WatsonxConnector(
         base_url="test.somedomain.com",
-        usr_name="test-user-1",
-        api_key="some_api_key_string"
+        user_name="test-user-1",
+        api_key="some_api_key_string",
+        model_id="some/model",
+        project_id="somelongprojectid"
     )
 
-    conn.set_system_prompt(i_system_prompt="SOMETHING")
-    conn.set_model_id(i_model_id="TEST_MODEL")
-    # print(conn.generate_auth_token())
-    print(conn.get_model_id())
+    conn.generate_text("What can you do?")
 
 
 if __name__ == '__main__':
